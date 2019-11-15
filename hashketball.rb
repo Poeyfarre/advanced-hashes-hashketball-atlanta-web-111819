c3df3bf1019f34e50f7352cf91dc2c143db7c092
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-15 19:49:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,45 +6,61 @@ def game_hash
       :colors => ["Black", "White"],
       :players => {
         "Alan Anderson" => {
-          :number =>
-          :shoe =>
-          :points =>
-          :rebounds =>
-          :assists =>
-          :steals =>
-          :blocks =>
-          :slam_dunks =>
+          :number => 0 
+          :shoe => 16
+          :points => 22
+          :rebounds => 12
+          :assists => 12
+          :steals => 3
+          :blocks => 1 
+          :slam_dunks => 1
         },
-        "Alan Anderson" => {
-          :number =>
-          :shoe =>
-          :points =>
-          :rebounds =>
-          :assists =>
-          :steals =>
-          :blocks =>
-          :slam_dunks =>
+        "Reggie Evans" => {
+          :number => 30
+          :shoe => 14
+          :points => 12
+          :rebounds => 12
+          :assists => 12
+          :steals => 12
+          :blocks =>12
+          :slam_dunks => 7
         },
-        "Alan Anderson" => {
-          :number =>
-          :shoe =>
-          :points =>
-          :rebounds =>
-          :assists =>
-          :steals =>
-          :blocks =>
-          :slam_dunks =>
+        "Brook Lopez" => {
+          :number => 11
+          :shoe => 17
+          :points => 17
+          :rebounds => 19
+          :assists => 10 
+          :steals => 3 
+          :blocks => 1 
+          :slam_dunks => 15
         },
-        "Alan Anderson" => {
-          :number =>
-          :shoe =>
-          :points =>
-          :rebounds =>
-          :assists =>
-          :steals =>
-          :blocks =>
-          :slam_dunks =>
+        "Mason Plumlee" => {
+          :number => 1 
+          :shoe => 19
+          :points => 26
+          :rebounds => 11
+          :assists => 6 
+          :steals => 3 
+          :blocks => 8
+          :slam_dunks => 5
         },
+        "Jason Terry" => {
+          :number => 31
+          :shoe => 15
+          :points => 19
+          :rebounds => 2 
+          :assists => 2 
+          :steals => 4  
+          :blocks => 11
+          :slam_dunks => 1 
+        }
+      }
+    },
+    :away => {
+      :team_name => "Charlotte Hornets"
+      :colors => ["Turqoise", "Purple"],
+      :players => {
         
       }
     }
