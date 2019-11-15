f835d46a7f2b0d634e8382c67176f8aab5ac8983
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-15 20:07:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -22,7 +22,7 @@ def game_hash
           :rebounds => 12
           :assists => 12
           :steals => 12
-          :blocks =>12
+          :blocks => 12
           :slam_dunks => 7
         },
         "Brook Lopez" => {
