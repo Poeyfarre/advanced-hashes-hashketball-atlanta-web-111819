ef50a776e80f70a2dcbeb01653bfbe6ba6e89d95
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-15 20:34:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,6 +118,9 @@ def game_hash
   }
 end
 def num_points_scored(player_name)
+  game_hash.values.find do |team_name|
+    team_name[:players].find do |players|
+    return players[:points] if players.has_value? (player_name)
   
 end
 
