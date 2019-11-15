384359bd474e90d0bdae0d0cf7e37ccb844a0453
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-15 19:41:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,24 @@
 def game_hash
   
-  
+  hash_ball = {
+    :home =>{
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
+        "Alan Anderson" => {
+          :number =>
+          :shoe =>
+          :points =>
+          :rebounds =>
+          :assists =>
+          :steals =>
+          :blocks =>
+          :slam_dunks =>
+        },
+        
+      }
+    }
+  }
 
 
 
